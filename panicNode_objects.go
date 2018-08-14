fd4744c58d31014fa64dc3d871ef8afad2d7b2b0
Dominic Pettifer <sironfoot@gmail.com>
2018-08-14 23:53:29 +0100

    fixed: slice reference bugs.

--- panicNode_objects.go
+++ panicNode_objects.go
@@ -2,64 +2,90 @@ package jscraper
 
 // ObjectP ...
 func (n *PanicNode) ObjectP(fieldName string) *PanicNode {
-	v, err := n.Node.ObjectP(fieldName)
+	node, err := n.Node.ObjectP(fieldName)
 	panicOnErr(err)
 
-	return v.UsePanicAPI()
+	if node == nil {
+		return nil
+	}
+	return node.UsePanicAPI()
 }
 
 // Object ...
 func (n *PanicNode) Object(fieldName string) PanicNode {
-	v, err := n.Node.Object(fieldName)
+	node, err := n.Node.Object(fieldName)
 	panicOnErr(err)
 
-	return *v.UsePanicAPI()
+	return *node.UsePanicAPI()
 }
 
 // ArrayPOfObjectsP ...
 func (n *PanicNode) ArrayPOfObjectsP(fieldName string) []*PanicNode {
-	vs, err := n.Node.ArrayPOfObjectsP(fieldName)
+	nodes, err := n.Node.ArrayPOfObjectsP(fieldName)
 	panicOnErr(err)
 
-	var pv []*PanicNode
-	for _, v := range vs {
-		pv = append(pv, v.UsePanicAPI())
+	if nodes == nil {
+		return nil
 	}
-	return pv
+
+	var pNodes []*PanicNode
+	for i := range nodes {
+		node := nodes[i]
+
+		if node == nil {
+			pNodes = append(pNodes, nil)
+		} else {
+			pNodes = append(pNodes, node.UsePanicAPI())
+		}
+	}
+	return pNodes
 }
 
 // ArrayPOfObjects ...
 func (n *PanicNode) ArrayPOfObjects(fieldName string) []PanicNode {
-	vs, err := n.Node.ArrayPOfObjects(fieldName)
+	nodes, err := n.Node.ArrayPOfObjects(fieldName)
 	panicOnErr(err)
 
-	var pv []PanicNode
-	for _, v := range vs {
-		pv = append(pv, *v.UsePanicAPI())
+	if nodes == nil {
+		return nil
 	}
-	return pv
+
+	var pNodes []PanicNode
+	for i := range nodes {
+		pNode := nodes[i].UsePanicAPI()
+		pNodes = append(pNodes, *pNode)
+	}
+	return pNodes
 }
 
 // ArrayOfObjectsP ...
 func (n *PanicNode) ArrayOfObjectsP(fieldName string) []*PanicNode {
-	vs, err := n.Node.ArrayOfObjectsP(fieldName)
+	nodes, err := n.Node.ArrayOfObjectsP(fieldName)
 	panicOnErr(err)
 
-	var pv []*PanicNode
-	for _, v := range vs {
-		pv = append(pv, v.UsePanicAPI())
+	var pNodes []*PanicNode
+	for i := range nodes {
+		node := nodes[i]
+
+		if node == nil {
+			pNodes = append(pNodes, nil)
+		} else {
+			pNodes = append(pNodes, node.UsePanicAPI())
+		}
 	}
-	return pv
+	return pNodes
 }
 
 // ArrayOfObjects ...
 func (n *PanicNode) ArrayOfObjects(fieldName string) []PanicNode {
-	vs, err := n.Node.ArrayOfObjects(fieldName)
+	nodes, err := n.Node.ArrayOfObjects(fieldName)
 	panicOnErr(err)
 
-	var pv []PanicNode
-	for _, v := range vs {
-		pv = append(pv, *v.UsePanicAPI())
+	var pNodes []PanicNode
+	for i := range nodes {
+		pNode := nodes[i].UsePanicAPI()
+		pNodes = append(pNodes, *pNode)
 	}
-	return pv
+
+	return pNodes
 }
